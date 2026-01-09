ee6ce475d79e547e55467171aef66c22229574a3
sandhya <p91sandhya@gmail.com>
2026-01-09 13:29:27 +0545

    --

--- Backend/server.js
+++ Backend/server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(frontendDist));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
