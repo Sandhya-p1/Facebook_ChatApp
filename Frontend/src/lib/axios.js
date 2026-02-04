bd4cc6484453bd029a52a77f18c1da0158f327dd
sandhya <p91sandhya@gmail.com>
2026-02-04 14:48:55 +0545

    changes made to axios file

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,10 +1,20 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? "http://localhost:8000/api"
+//       : "/api",
+//   headers: { "Content-Type": "application/json" },
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:8000/api"
-      : "/api",
-  headers: { "Content-Type": "application/json" },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
   withCredentials: true,
 });
